737b3d9c23ee79bffbe1a3f83eb5dafae53cc7dd
senpooja <poojasen744055@gmail.com>
2022-07-15 16:25:13 +0530

    added empwageswitch files

--- empsystem/empsystem/Program.cs
+++ empsystem/empsystem/Program.cs
@@ -1,25 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class program
+{ 
+public const int IsFullTime = 1;
+public const int IsPartTime = 2;
+public const int EmpRatPerHr = 20;
+    static void Main(string[] args)
+    {
 
-Console.WriteLine("welcome to Employee Management app");
-int IsFullTime = 1;
-int IsPartTime = 2;
-int EmpRatePerhr = 20;
-int empHrs = 0;
-int empWage = 0;
 
-Random random = new Random();
-int EmpCheck = random.Next(0, 2);
-if (EmpCheck == IsFullTime)
-{
-    empHrs = 8;
-}
-else if (EmpCheck == IsPartTime)
-{
-    empHrs = 4;
-}
-else
-{
-    empHrs = 0;
-}
-empWage = empHrs * EmpRatePerhr;
-Console.WriteLine("Emp Wage :" + empWage);
+        int empHrs = 0;
+        int EmpWage = 0;
+        Random random = new Random();
+
+        int EmpCheck = random.Next(0, 3);
+        switch (EmpCheck)
+        {
+            case IsPartTime: //IsPartTime=2//
+                empHrs = 4;
+                break;
+            case IsFullTime: //IsFullTime=1//
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+                break;
+        }
+        EmpWage = empHrs * EmpRatPerHr;
+        Console.WriteLine("Employees wage is: " + EmpWage);
+    }
+    }
