d3b30dd14c5235aef70447b9493477e8fbc7aaac
senpooja <poojasen744055@gmail.com>
2022-07-15 12:50:37 +0530

    added empattendence files

--- empsystem/empsystem/Program.cs
+++ empsystem/empsystem/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("welcome to Employee Management app");
+int IsFullTime = 1;
+Random random = new Random();
+int EmpCheck = random.Next(0, 2);
+if (EmpCheck == IsFullTime)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
